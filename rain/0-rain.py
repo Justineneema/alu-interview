8b6105752cc39b1cf9cdb659867a62031f5db4cc
Justineneema <j.neema@alustudent.com>
2025-05-28 09:32:50 +0200

    rain done

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -8,25 +8,25 @@ not walls, meaning they will not retain water.
 If the list is empty return 0.
 """
 
+
 def rain(walls):
     if len(walls) < 3:
         return 0
-    
+
     total_water = 0
     left = 0
-    right = 0
+    right = len(walls) - 1
     left_max = walls[left]
     right_max = walls[right]
 
     while left < right:
         if walls[left] <= walls[right]:
             left_max = max(left_max, walls[left])
-            total_water += left_max[left]
+            total_water += left_max - walls[left]
             left += 1
-
         else:
             right_max = max(right_max, walls[right])
             total_water += right_max - walls[right]
-            rught -= 1
+            right -= 1
 
-    return total_water        
+    return total_water
